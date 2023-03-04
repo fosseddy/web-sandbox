34ee20ca6dae600b06565db760b437bec545a6d7
fosseddy <markfosseddy@gmail.com>
2023-03-04 16:21:29 +0500

    dictapp: ready, set, go!

---
+++ fntedmtr/dictapp/src/app.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+
+    computed: {
+    },
+
+    methods: {
+    }
+};
+</script>
+
+<template>
+<div>
+    <h1>hello, world</h1>
+</div>
+</template>
+
+<style scoped>
+</style>
