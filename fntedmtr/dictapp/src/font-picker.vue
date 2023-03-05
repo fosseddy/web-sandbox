3b29643bbccda8c2f42edc94eb022281ce27e461
fosseddy <markfosseddy@gmail.com>
2023-03-05 20:23:49 +0500

    reorganize

---
+++ fntedmtr/dictapp/src/font-picker.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    data() {
+        return {
+            font: "Serif"
+        };
+    },
+
+    watch: {
+        font(cur, old) {
+            document.body.classList.replace(
+                `font-picker--${old}`,
+                `font-picker--${cur}`
+            );
+        }
+    },
+
+    created() {
+        document.body.classList.add(`font-picker--${this.font}`);
+    }
+};
+</script>
+
+<template>
+    <select v-model="font">
+        <option>Serif</option>
+        <option>Mono</option>
+        <option>Sans-Serif</option>
+    </select>
+</template>
+
+<style scoped>
+</style>
+
+<style>
+.font-picker--Serif {
+    font-family: serif;
+}
+
+.font-picker--Mono {
+    font-family: monospace;
+}
+
+.font-picker--Sans-Serif {
+    font-family: sans-serif;
+}
+</style>
---
+++ fntedmtr/dictapp/src/theme-toggle.vue
@@ -0,0 +1,59 @@
+<script>
+import Icon from "@/icons/icon.vue";
+
+export default {
+    components: { Icon },
+
+    data() {
+        return {
+            theme: "light"
+        };
+    },
+
+    computed: {
+        themeIcon() {
+            return this.theme == "light" ? "moon" : "sun";
+        }
+    },
+
+    watch: {
+        theme(cur, old) {
+            document.body.classList.replace(`theme--${old}`, `theme--${cur}`);
+        }
+    },
+
+    created() {
+        document.body.classList.add(`theme--${this.theme}`);
+    }
+};
+</script>
+
+<template>
+    <div class="theme-picker">
+        <input type="checkbox"
+               v-model="theme"
+               true-value="dark"
+               false-value="light"
+        >
+        <Icon :name="themeIcon" />
+    </div>
+</template>
+
+<style scoped>
+.theme-picker {
+    display: flex;
+    align-items: center;
+}
+</style>
+
+<style>
+.theme--light {
+    --color-bg: white;
+    --color-fg: black;
+}
+
+.theme--dark {
+    --color-bg: black;
+    --color-fg: white;
+}
+</style>
