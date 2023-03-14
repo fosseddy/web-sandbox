48c2413609116aec0c56547d9f4ab326f1bf65b0
fosseddy <markfosseddy@gmail.com>
2023-03-14 21:07:02 +0500

    msf: ready, set, go!

---
+++ fntedmtr/multi-step-form/src/app.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    components: {},
+
+    data() {
+        return {
+        };
+    },
+
+    computed: {
+    },
+
+    watch: {
+    },
+
+    methods: {
+    }
+
+    //created() {},
+    //mounted() {},
+    //updated() {},
+    //unmounted() {},
+};
+</script>
+
+<template>
+    <div>
+        <h1>hello, world</h1>
+    </div>
+</template>
+
+<style scoped>
+</style>
