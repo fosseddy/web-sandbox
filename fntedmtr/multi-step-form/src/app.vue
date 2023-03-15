6d36afaf4c706099917db4ab87257a8aca8d576b
fosseddy <markfosseddy@gmail.com>
2023-03-15 05:05:48 +0500

    transition

--- fntedmtr/multi-step-form/src/app.vue
+++ fntedmtr/multi-step-form/src/app.vue
@@ -4,11 +4,9 @@ export default {
 
     data() {
         return {
+            transition: "slide-right",
             tabnum: 4,
-            tab: 1,
-            name: "",
-            email: "",
-            phone: ""
+            tab: 1
         };
     },
 
@@ -22,13 +20,20 @@ export default {
         nextTab() {
             if (this.tab < this.tabnum) {
                 this.tab += 1;
+                this.transition = "slide-right";
+
             }
         },
 
         prevTab() {
             if (this.tab > 1) {
                 this.tab -= 1;
+                this.transition = "slide-left";
             }
+        },
+
+        submit() {
+            console.log("submitting data...");
         }
     }
 };
@@ -42,65 +47,72 @@ export default {
     </header>
 
     <main>
-        <section :class="{'tab-left': tab > 1 }">
-            <h2>Personal info</h2>
-            <p>Please provide your name, email, address, and phone number.</p>
-            <form>
-                <div>
-                    <label for="name">Name</label>
-                    <input id="name" v-model="name">
-                </div>
-                <div>
-                    <label for="email">Email Address</label>
-                    <input id="email" v-model="email">
-                </div>
-                <div>
-                    <label for="phone">Phone Number</label>
-                    <input id="phone" v-model="phone">
-                </div>
-            </form>
-        </section>
-        <section :class="{'tab-right': tab < 2, 'tab-left': tab > 2 }">
-            <h2>Select your plan</h2>
-            <p>You have the option of monthly or yearly billing.</p>
-            <form>
-                <div>
-                    <input id="arcade" type="checkbox">
-                    <label for="arcade">Arcade</label>
-                    <p>$9/mo</p>
-                </div>
-                <div>
-                    <input id="advanced" type="checkbox">
-                    <label for="advanced">Advanced</label>
-                    <p>$12/mo</p>
-                </div>
-                <div>
-                    <input id="pro" type="checkbox">
-                    <label for="pro">Pro</label>
-                    <p>$15/mo</p>
-                </div>
-            </form>
-        </section>
-        <section :class="{'tab-right': tab < 3, 'tab-left': tab > 3 }">
-            <h2>Tab 3</h2>
-            <p>This is Tab 3.</p>
-        </section>
-        <section :class="{'tab-right': tab < 4}">
-            <h2>Tab 4</h2>
-            <p>This is Tab 4.</p>
-        </section>
+        <Transition :name="transition">
+            <section v-if="tab == 1">
+                <h2>Personal info</h2>
+                <p>Please provide your name, email, address, and phone number.</p>
+                <form>
+                    <div>
+                        <label for="name">Name</label>
+                        <input id="name">
+                    </div>
+                    <div>
+                        <label for="email">Email Address</label>
+                        <input id="email">
+                    </div>
+                    <div>
+                        <label for="phone">Phone Number</label>
+                        <input id="phone">
+                    </div>
+                </form>
+            </section>
+
+            <section v-else-if="tab == 2">
+                <h2>Select your plan</h2>
+                <p>You have the option of monthly or yearly billing.</p>
+                <form>
+                    <div>
+                        <input id="arcade" type="checkbox">
+                        <label for="arcade">Arcade</label>
+                        <p>$9/mo</p>
+                    </div>
+                    <div>
+                        <input id="advanced" type="checkbox">
+                        <label for="advanced">Advanced</label>
+                        <p>$12/mo</p>
+                    </div>
+                    <div>
+                        <input id="pro" type="checkbox">
+                        <label for="pro">Pro</label>
+                        <p>$15/mo</p>
+                    </div>
+                </form>
+            </section>
+
+            <section v-else-if="tab == 3">
+                <h2>Tab 3</h2>
+                <p>This is Tab 3.</p>
+            </section>
+
+            <section v-else>
+                <h2>Tab 4</h2>
+                <p>This is Tab 4.</p>
+            </section>
+        </Transition>
     </main>
 
     <footer>
-        <button @click="prevTab" :class="{hidden: tab == 1}">Go Back</button>
-        <button @click="nextTab">Next Step</button>
+        <div>
+            <button v-if="tab > 1" @click="prevTab">Go Back</button>
+        </div>
+        <button v-if="tab < tabnum" @click="nextTab">Next Step</button>
+        <button v-else @click="submit">Confirm</button>
     </footer>
 </template>
 
 <style scoped>
 main {
     background: gray;
-    position: relative;
 }
 
 header {
@@ -111,11 +123,10 @@ header {
 section {
     background: white;
     padding: 1rem;
-    position: absolute;
+    position: relative;
     top: -3rem;
     left: 1rem;
     right: 1rem;
-    transition: transform 350ms ease;
 }
 
 footer {
@@ -125,20 +136,25 @@ footer {
     justify-content: space-between;
 }
 
-.hidden {
-    visibility: hidden;
-}
-
 .active {
     background: yellow;
 }
 
-.tab-right {
-    transform: translateX(calc(100% + 1rem));
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+    transition: transform 300ms ease;
 }
 
-.tab-left {
-    transform: translateX(calc(-100% - 1rem));
+.slide-right-enter-from,
+.slide-left-leave-to {
+    transform: translateX(calc(100% + 5rem));
+}
+
+.slide-right-leave-to,
+.slide-left-enter-from {
+    transform: translateX(calc(-100% - 5rem));
 }
 </style>
 
@@ -162,11 +178,11 @@ html,
 body,
 #app {
     height: 100%;
-    overflow-x: hidden;
 }
 
 #app {
     display: grid;
     grid-template-rows: 10rem 1fr 4rem;
+    overflow-x: hidden;
 }
 </style>
