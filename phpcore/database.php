15f0250991b9485ae50f8fce4f19e7ebb90feb79
fosseddy <markfosseddy@gmail.com>
2023-07-10 12:41:31 +0500

    move framework out, change database module

---
+++ phpcore/database.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database;
+
+use PDO;
+
+function connect($host, $dbname, $user, $pass)
+{
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $db;
+}
+
+class Connection
+{
+    public $pdo;
+    public $stmt;
+
+    function __construct($host, $dbname, $user, $pass)
+    {
+        $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    function exec($q, $vals = [])
+    {
+        $this->stmt = $this->pdo->prepare($q);
+        $this->stmt->execute($vals);
+    }
+
+    function query($q, $vals = [], $container = null)
+    {
+        $this->exec($q, $vals);
+
+        if ($container)
+        {
+            $mode = PDO::FETCH_INTO;
+            if (gettype($container) === "string") $mode = PDO::FETCH_CLASS;
+            $this->stmt->setFetchMode($mode, $container);
+        }
+    }
+
+    function query_one($q, $vals = [], $container = null)
+    {
+        $this->query($q, $vals, $container);
+        return $this->stmt->fetch();
+    }
+
+    function query_many($q, $vals = [], $container = null)
+    {
+        $this->query($q, $vals, $container);
+        return $this->stmt->fetchAll();
+    }
+}
