b2a0286cf080b7864629b7cc85b8d087ddf9e2f8
fosseddy <markfosseddy@gmail.com>
2023-07-14 11:25:54 +0500

    remove core

--- php/core/path.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace path;
-
-// NOTE(art): BASE_DIR must be defined by user
-function from_base(string ...$path): string
-{
-    return join(DIRECTORY_SEPARATOR, [BASE_DIR, ...$path]);
-}
